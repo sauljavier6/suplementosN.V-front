b165592d9587d267da90ba46ebbee6bff11eef18
Saul Javier Peralta Gomez <125310857+sauljavier6@users.noreply.github.com>
2026-03-21 01:33:11 -0700

    cambios en logo

--- src/components/home/banneer/Banner.jsx
+++ src/components/home/banneer/Banner.jsx
@@ -1,14 +1,16 @@
 function Banner() {
   return (
-    <div className="w-full h-[60vh] sm:h-[70vh] md:h-[90vh] lg:h-[115vh] overflow-hidden">
+    <div className="w-full h-[60vh] sm:h-[70vh] md:h-[85vh] lg:h-[115vh] xl:h-[115vh] overflow-hidden">
       <img
         src="/bg/banner.png"
         alt="banner"
         className="
-          w-full h-full 
-          object-cover 
-          object-[center_top] 
-          sm:object-center 
+          h-full 
+          w-auto 
+          object-contain 
+          object-top
+          sm:w-full sm:h-full sm:object-cover
+          lg:object-center
         "
       />
     </div>
